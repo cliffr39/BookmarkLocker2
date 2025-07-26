1040a2986e16534d942a503916f0014cdcc0c75c
cliffr39 <86836751+cliffr39@users.noreply.github.com>
2025-07-26 14:47:31 -0400

    Update .gitignore

--- app/build/generated/hilt/component_trees/release/dagger/hilt/internal/processedrootsentinel/codegen/_com_bookmark_locker_BookmarkApplication.java
+++
@@ -1,9 +0,0 @@
-package dagger.hilt.internal.processedrootsentinel.codegen;
-
-import dagger.hilt.internal.processedrootsentinel.ProcessedRootSentinel;
-
-@ProcessedRootSentinel(
-    roots = "com.bookmark.locker.BookmarkApplication"
-)
-public final class _com_bookmark_locker_BookmarkApplication {
-}
